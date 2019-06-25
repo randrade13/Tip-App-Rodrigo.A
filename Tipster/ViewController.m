ab307d68895ded4cbee3805ae13c31d09372cc9a
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-06-25 16:31:11 -0700

    Required tasks complete without readme

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -9,8 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UIView *tipMenu;
 
 @end
+
 
 @implementation ViewController
 
@@ -18,6 +24,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Close Keyboard on Screen");
+    [self.view endEditing:YES];
+}
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.20), @(0.22)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue] ;
+    
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.tipMenu.alpha = 1;
+    }];
+}
+- (IBAction)onEditingEnd:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.tipMenu.alpha = 0;
+    }];
+}
+
 
 
 @end
