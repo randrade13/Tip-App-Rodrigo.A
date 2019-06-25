72295056f59e051d3e23663ce4de016d7f833348
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-06-25 13:49:03 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by rodrigoandrade on 6/25/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipster/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by rodrigoandrade on 6/25/19.
+//  Copyright © 2019 rodrigoandrade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
